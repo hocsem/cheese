32214cda850cd46f7f9f348e2356c724d952ab1c
hocsem <120021690+hocsem@users.noreply.github.com>
2023-01-11 12:14:46 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,66 @@
+let almacenDeQuesos = {
+    {
+        id: 1,
+        nombre: "Queso rojo de cabra",
+        descripcion: " Delicioso queso artesanal de cabra madurado durante siete meses en los valles de Aturias occidental",
+        precio: 9.10,
+        image: scr = "quesorojocabra.jpg",
+
+    }
+    {
+        id: 2,
+        nombre: "Queso de oveja Abredo",
+        descripcion: " Creado con la leche de ovejas que pastan libremente en Abredo, un queso con un cuerpo y texturas características",
+        precio: 10.00,
+        image: scr = "quesoovejaabredo.jpg",
+
+    }  
+    {
+        id: 3,
+        nombre: "Queso maduro tres leches",
+        descripcion: " Mezcla de tres leches para un queso muy maduro con un aroma carismático y un sabor único",
+        precio: 12.30,
+        image: scr = "quesomadurotresleches.jpg",
+
+    }
+    {
+        id: 4,
+        nombre: "Queso de oveja La Colllada",
+        descripcion: " La Colllada es un precioso pueblo donde pastan las ovejas que producen la leche característica que imprime una personalidad exclusiva a nuestro queso",
+        precio: 14.90,
+        image: scr = "quesodeovejalacolllada.jpg",
+
+    }
+    {
+        id: 5,
+        nombre: "Queso de Coaña",
+        descripcion: " Gracias a la tradición quesera de la Coaña conseguimos este fabuloso queso suave y cremoso de vacas asturianas",
+        precio: 9.50,
+        image: scr = "quesodecoaña.jpg",
+
+    }
+    {
+        id: 6,
+        nombre: "Queso cabrales",
+        descripcion: " Enigmático y poderoso uno de los quesos más particulares de Asturias, una experiencia para el paladar reconocido a nivel internacional",
+        precio: 17.10,
+        image: scr = "quesocabrales.jpg",
+
+    }
+    {
+        id: 7,
+        nombre: "Queso azul de vaca",
+        descripcion: " El rey cremoso y de sabor terrestre que proviene de las vacas occidentales que pastan en los acantilados de la bella costa Asturiana",
+        precio: 14.00,
+        image: scr = "quesoazulvaca.jpg",
+
+    }
+    {
+        id: 8,
+        nombre: "Queso Afuegal Pitu",
+        descripcion: " Maravilloso queso elaborado según las tradiciones centenarias de la comarca dl Pitu",
+        precio: 12.30,
+        image: scr = "quesoafuegalpitu.jpg",
+
+    }
+}
